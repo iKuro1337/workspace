0dfa066dd4479feafbeb532ec2e9a5eb54fb45b6
iKuro1337 <ikuro1337@gmail.com>
2016-04-15 13:58:22 +0200

    initial commit

---
+++ AJAX/build/generic/platform/cores/esp8266/WMath.cpp.d
@@ -0,0 +1,4 @@
+generic/platform/cores/esp8266/WMath.cpp.o: \
+ d:/Users/simtech/.arduinocdt/packages/esp8266/hardware/esp8266/esp8266/2.1.0/cores/esp8266/WMath.cpp \
+ d:/Users/simtech/.arduinocdt/packages/esp8266/hardware/esp8266/esp8266/2.1.0/cores/esp8266/esp8266_peri.h \
+ d:\Users\simtech\.arduinocdt\packages\esp8266\hardware\esp8266\esp8266\2.1.0/tools/sdk/include/c_types.h
---
+++ AJAX/build/generic/platform/cores/esp8266/core_esp8266_noniso.c.d
@@ -0,0 +1,6 @@
+generic/platform/cores/esp8266/core_esp8266_noniso.c.o: \
+ d:/Users/simtech/.arduinocdt/packages/esp8266/hardware/esp8266/esp8266/2.1.0/cores/esp8266/core_esp8266_noniso.c \
+ d:/Users/simtech/.arduinocdt/packages/esp8266/hardware/esp8266/esp8266/2.1.0/cores/esp8266/stdlib_noniso.h \
+ d:\Users\simtech\.arduinocdt\packages\esp8266\hardware\esp8266\esp8266\2.1.0/tools/sdk/include/ets_sys.h \
+ d:\Users\simtech\.arduinocdt\packages\esp8266\hardware\esp8266\esp8266\2.1.0/tools/sdk/include/c_types.h \
+ d:\Users\simtech\.arduinocdt\packages\esp8266\hardware\esp8266\esp8266\2.1.0/tools/sdk/include/eagle_soc.h
